060e38fb9ade07d2ee5451e8f662943cc3e75d4a
Ayama11 <ayamalla882@gmail.com>
2024-04-04 16:09:00 +0300

    add appbar to detiles book view

--- lib/Feature/home/presentions/views/widgets/book_detiles_view.dart
+++ lib/Feature/home/presentions/views/widgets/book_detiles_view.dart
@@ -1,10 +1,31 @@
+import 'package:bookapp/Feature/home/presentions/views/widgets/app_bar_w.dart';
+import 'package:bookapp/constes.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:iconsax/iconsax.dart';
 
 class BookDetilesView extends StatelessWidget {
   const BookDetilesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: kPadding30,
+        child: Column(children: [
+          AppBarW(
+              backgroundColor: Colors.transparent,
+              leadingiIcon: Icons.close,
+              leadingOnPressed: () {
+                GoRouter.of(context).pop();
+              },
+              actions: [
+                IconButton(
+                    onPressed: () {}, icon: const Icon(Iconsax.shopping_bag))
+              ])
+        ]),
+      )),
+    );
   }
 }
