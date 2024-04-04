8d73fc14c8cafb076245b13edf3b9539d5c4e47a
Ayama11 <ayamalla882@gmail.com>
2024-04-04 11:28:48 +0300

    Add book details view and navigation to it from best seller list view
    - Added BookDetilesView widget for displaying book details
    - Updated AppRouter to include a route for BookDetilesView
    - Implemented navigation to BookDetilesView from BestSellerListViewItem widget

--- lib/Feature/home/presentions/views/widgets/best_seller_listview_item.dart
+++ lib/Feature/home/presentions/views/widgets/best_seller_listview_item.dart
@@ -1,59 +1,66 @@
 import 'package:bookapp/Feature/home/presentions/views/widgets/book_rate.dart';
+import 'package:bookapp/core/utils/app_router.dart';
 import 'package:bookapp/core/utils/assets.dart';
 import 'package:bookapp/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 130,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.4 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: Colors.amber,
-                  image: const DecorationImage(
-                      fit: BoxFit.fill, image: AssetImage(AssetsData.test))),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetilesView);
+      },
+      child: SizedBox(
+        height: 130,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.4 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.amber,
+                    image: const DecorationImage(
+                        fit: BoxFit.fill, image: AssetImage(AssetsData.test))),
+              ),
             ),
-          ),
-          const SizedBox(width: 30),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .5,
-                  child: const Text(
-                    'Herry potr and got fire',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: Styles.textStyle21,
-                  ),
-                ),
-                const SizedBox(height: 3),
-                const Text('J.K. Rorwing', style: Styles.textStyle14),
-                const SizedBox(height: 3),
-                Row(
-                  children: [
-                    Text(
-                      '19.9 @',
-                      style: Styles.textStyle18
-                          .copyWith(fontWeight: FontWeight.bold),
+            const SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: const Text(
+                      'Herry potr and got fire',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.textStyle21,
                     ),
-                    const Spacer(),
-                    const BookRate()
-                  ],
-                ),
-              ],
-            ),
-          )
-        ],
+                  ),
+                  const SizedBox(height: 3),
+                  const Text('J.K. Rorwing', style: Styles.textStyle14),
+                  const SizedBox(height: 3),
+                  Row(
+                    children: [
+                      Text(
+                        '19.9 @',
+                        style: Styles.textStyle18
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      const BookRate()
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,11 @@
+import 'package:bookapp/Feature/home/presentions/views/home_view.dart';
+import 'package:bookapp/Feature/home/presentions/views/widgets/book_detiles_view.dart';
 import 'package:bookapp/Feature/splash/prisention/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/HomeView';
-
+  static const kBookDetilesView = '/BookDetilesView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -12,7 +14,11 @@ abstract class AppRouter {
       ),
       GoRoute(
         path: kHomeView,
-        builder: (context, state) => const SplashView(),
+        builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: kBookDetilesView,
+        builder: (context, state) => const BookDetilesView(),
       ),
     ],
   );
