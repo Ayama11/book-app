4dfdc22b46df511a642dac68539231ff513bb398
Ayama11 <ayamalla882@gmail.com>
2024-04-04 16:29:15 +0300

    add titel and subtitel to bookdetiles

--- lib/Feature/home/presentions/views/widgets/book_detiles_view.dart
+++ lib/Feature/home/presentions/views/widgets/book_detiles_view.dart
@@ -1,6 +1,7 @@
 import 'package:bookapp/Feature/home/presentions/views/widgets/app_bar_w.dart';
 import 'package:bookapp/Feature/home/presentions/views/widgets/custom_book_item.dart';
 import 'package:bookapp/constes.dart';
+import 'package:bookapp/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:iconsax/iconsax.dart';
@@ -29,6 +30,17 @@ class BookDetilesView extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: const CustomBookItem(),
           ),
+          const SizedBox(height: 45),
+          const Text('The Jungle Book', style: Styles.textStyle30),
+          const SizedBox(height: 6),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rouding Kliping',
+              style: Styles.textStyle18.copyWith(
+                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+            ),
+          )
         ]),
       ),
     );
