9fcf16d6f56fb45c62946225d70a099187977f04
Ayama11 <ayamalla882@gmail.com>
2024-04-04 22:22:07 +0300

    refactor code

---
+++ lib/Feature/home/presentions/views/widgets/body_detiels_book_view.dart
@@ -0,0 +1,44 @@
+import 'package:bookapp/Feature/home/presentions/views/widgets/section_book_detiles.dart';
+import 'package:bookapp/Feature/home/presentions/views/widgets/section_similer_book.dart';
+import 'package:bookapp/constes.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:iconsax/iconsax.dart';
+
+import 'app_bar_w.dart';
+
+class DetilesBookBody extends StatelessWidget {
+  const DetilesBookBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: kPadding30,
+            child: Column(children: [
+              AppBarW(
+                  backgroundColor: Colors.transparent,
+                  leadingiIcon: Icons.close,
+                  leadingOnPressed: () {
+                    GoRouter.of(context).pop();
+                  },
+                  actions: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Iconsax.shopping_bag))
+                  ]),
+              const SizedBox(height: 20),
+              const BookDetilesSection(),
+              const Expanded(child: SizedBox(height: 24)),
+              const SectionSimilerBookListView(),
+              const SizedBox(height: 46),
+            ]),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/Feature/home/presentions/views/widgets/book_detiles_view.dart
+++ lib/Feature/home/presentions/views/widgets/book_detiles_view.dart
@@ -1,36 +1,13 @@
-import 'package:bookapp/Feature/home/presentions/views/widgets/app_bar_w.dart';
-import 'package:bookapp/Feature/home/presentions/views/widgets/section_book_detiles.dart';
-import 'package:bookapp/Feature/home/presentions/views/widgets/section_similer_book.dart';
-import 'package:bookapp/constes.dart';
+import 'package:bookapp/Feature/home/presentions/views/widgets/body_detiels_book_view.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:iconsax/iconsax.dart';
 
 class BookDetilesView extends StatelessWidget {
   const BookDetilesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: kPadding30,
-        child: Column(children: [
-          AppBarW(
-              backgroundColor: Colors.transparent,
-              leadingiIcon: Icons.close,
-              leadingOnPressed: () {
-                GoRouter.of(context).pop();
-              },
-              actions: [
-                IconButton(
-                    onPressed: () {}, icon: const Icon(Iconsax.shopping_bag))
-              ]),
-          const SizedBox(height: 20),
-          const BookDetilesSection(),
-          const SizedBox(height: 24),
-          const SectionSimilerBookListView()
-        ]),
-      ),
+    return const Scaffold(
+      body: DetilesBookBody(),
     );
   }
 }
