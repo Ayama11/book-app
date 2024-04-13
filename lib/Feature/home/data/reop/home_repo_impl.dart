35c38dfe5e4b3dccb5a0bbad53b8cfe1ada952a4
Ayama11 <ayamalla882@gmail.com>
2024-04-13 14:51:55 +0300

    Update fetchBestSellerBooks method to fetchNewsBooks and implement API call for fetching newest programming books
    - Updated the fetchBestSellerBooks method in HomeRepo interface to fetchNewsBooks
    - Implemented API call in HomeRepoImpl to fetch newest programming books
    - Added new ServiecesFailure class to handle API service failures

--- lib/Feature/home/data/reop/home_repo_impl.dart
+++ lib/Feature/home/data/reop/home_repo_impl.dart
@@ -1,12 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dartz/dartz.dart';
+
 import 'package:bookapp/Feature/home/data/models/book/book.dart';
 import 'package:bookapp/Feature/home/data/reop/home_repo.dart';
 import 'package:bookapp/core/errors/failure.dart';
-import 'package:dartz/dartz.dart';
+import 'package:bookapp/core/utils/api_services.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final Api api;
+  HomeRepoImpl({required this.api});
   @override
-  Future<Either<Failure, List<Book>>> fetchBestSellerBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<Book>>> fetchNewsBooks() async {
+    try {
+      var data = await api.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&q=subject:programming&Sorting=newest');
+      List<Book> books = [];
+      for (var item in data['items']) {
+        books.add(Book.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServiecesFailure());
+    }
   }
 
   @override
