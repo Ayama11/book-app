31f165e5c85653fde3e3cde46496ff3ede3c6687
Ayama11 <ayamalla882@gmail.com>
2024-04-13 21:51:18 +0300

    Refactor FeuturedBookCubit to fetch and emit featured books
    Add method to fetch featured books from HomeRepo and emit success or failure
    based on the result. Also, import necessary dependencies and ignore linting
    rules.

--- lib/Feature/splash/prisention/manger/futured_book/feutured_book_cubit.dart
+++ lib/Feature/splash/prisention/manger/futured_book/feutured_book_cubit.dart
@@ -1,9 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:ffi';
+
 import 'package:bloc/bloc.dart';
-import 'package:bookapp/Feature/home/data/models/book/book.dart';
 import 'package:equatable/equatable.dart';
+
+import 'package:bookapp/Feature/home/data/models/book/book.dart';
+import 'package:bookapp/Feature/home/data/reop/home_repo.dart';
 
 part 'feutured_book_state.dart';
 
 class FeuturedBookCubit extends Cubit<FeuturedBookState> {
-  FeuturedBookCubit() : super(FeuturedBookInitial());
+  FeuturedBookCubit(this.homeRepo) : super(FeuturedBookInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> featchFeaturedBooks() async {
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeuturedBookFailure(failure.errMessage));
+    }, (book) {
+      emit(FeuturedBookSuccess(book));
+    });
+  }
 }
