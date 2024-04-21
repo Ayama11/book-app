2548525c763050f798b342517c2097bb32e8c559
Ayama11 <ayamalla882@gmail.com>
2024-04-21 22:59:53 +0300

    Add cached network image widget to CustomBookItem
    Imported 'cached_network_image' package and replaced Container with CachedNetworkImage widget in CustomBookItem for better image loading performance.

--- lib/Feature/home/presentions/views/widgets/custom_book_item.dart
+++ lib/Feature/home/presentions/views/widgets/custom_book_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CustomBookItem extends StatelessWidget {
   const CustomBookItem({super.key, required this.imageUrl});
@@ -8,11 +9,10 @@ class CustomBookItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.8 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-                fit: BoxFit.fill, image: NetworkImage(imageUrl))),
+      child: CachedNetworkImage(
+        imageUrl: imageUrl,
+        fit: BoxFit.fill,
+        errorWidget: (context, url, error) => const Icon(Icons.error),
       ),
     );
   }
