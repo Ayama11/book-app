5858d8e1a38e933beba808ea16140e2dbb0e43e5
Ayama11 <ayamalla882@gmail.com>
2024-04-23 22:40:47 +0300

    Add SimilarBooksCubit and SimilarBooksState classes
    Added SimilarBooksCubit class to handle similar books logic and SimilarBooksState
    class to manage the state of similar books feature. This will help in fetching
    and displaying similar books in the application.

---
+++ lib/Feature/splash/prisention/manger/similer_books_cuibt/similar_books_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookapp/Feature/home/data/models/book/book.dart';
+import 'package:bookapp/Feature/home/data/reop/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'similar_books_state.dart';
+
+class SimilarBooksCubit extends Cubit<SimilarBooksState> {
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> featchSimilarBooks() async {
+    var result = await homeRepo.fetchSimilartBooks();
+    result.fold((failure) {
+      emit(SimilarBooksFailure(failure.errMessage));
+    }, (book) {
+      emit(SimilarBooksSuccess(book));
+    });
+  }
+}
